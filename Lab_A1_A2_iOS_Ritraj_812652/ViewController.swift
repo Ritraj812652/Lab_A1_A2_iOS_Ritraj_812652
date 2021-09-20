1773df46974d3b4f4b0ab98bd1a7dc9039a38aff
Ritraj <c0812652@mylambton.ca>
2021-09-20 04:02:24 -0400

    Initial Commit

---
+++ Lab_A1_A2_iOS_Ritraj_812652/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Ritraj_812652
+//
+//  Created by Ritraj Singh on 20/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
